304e9a5604bd1f7ff5704bc5ede102ede8f65a55
Oasis Pandey <oasis@Oasiss-Air-2.lan>
2025-05-16 16:48:25 -0500

    comparisions and string methods

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// DONT USE THESE COMPARISIONS, NOT GOOD FOR PRACTICE
+// ONLY COMPARE SAME DATATYPES
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === 
+
+console.log("2" === 2);
